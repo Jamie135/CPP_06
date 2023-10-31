e7548258aba72802f5b84ff3b3fe7cbb3e115b39
Pomprab BUREERAK <pbureera@made-f0Cr13s5.clusters.42paris.fr>
2023-10-31 16:42:03 +0100

    OK

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,1 +1,39 @@
 #pragma once
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <limits>
+#include <stdexcept>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+class ScalarConverter
+{
+private:
+	ScalarConverter();
+	ScalarConverter(ScalarConverter &copy);
+public:
+	ScalarConverter &operator=(const ScalarConverter &obj);
+	~ScalarConverter();
+	static void printInt(int i);
+	static void printChar(char c);
+	static void printSpecial(const std::string &val);
+	static void printFloat(float f);
+	static void printDouble(double d);
+	static void convert(const std::string &val);
+
+	class InvalidInputException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+};
+
+bool	isSpecial(const std::string &val);
+
+bool	isFloat(const std::string &val);
+
+bool	isDouble(const std::string &val);
+
+bool	isInteger(const std::string &val);
---
+++ ex00/main.cpp
@@ -0,0 +1,13 @@
+#include "ScalarConverter.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		try {
+			ScalarConverter::convert(argv[1]);
+		} catch (const std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+}
