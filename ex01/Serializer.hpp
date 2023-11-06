1716367dc4d0225aa305c0a2865909b13d2f9b9a
unknown <jamie.gworld@hotmail.com>
2023-11-06 20:46:37 +0700

    OK

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -8,7 +8,6 @@ typedef struct Data
 {
 	std::string	name;
 	size_t		age;
-	Data		*next;
 }				Data;
 
 class Serializer
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,43 +2,36 @@
 
 int main()
 {
+	Data ptr1;
+	ptr1.name = "Toto";
+	ptr1.age = 42;
+
 	Data ptr2;
-	ptr2.name = "Bob";
-	ptr2.age = 42;
-	ptr2.next = NULL;
-
-	Data ptr;
-	ptr.name = "Lily";
-	ptr.age = 42;
-	ptr.next = &ptr2;
-
+	ptr2.name = "Titi";
+	ptr2.age = 24;
 
 	std::cout << "Here is the original structs:" <<
-				"\n\taddress: " << &ptr <<
-				"\n\tname: " << ptr.name <<
-				"\n\tage: " << ptr.age <<
-				"\n\taddress next: " << ptr.next <<
+				"\n\taddress: " << &ptr1 <<
+				"\n\tname: " << ptr1.name <<
+				"\n\tage: " << ptr1.age <<
 	std::endl;
 	std::cout << "\taddress ptr2: " << &ptr2 <<
 				"\n\tname: " << ptr2.name <<
 				"\n\tage: " << ptr2.age <<
-				"\n\taddress next: " << ptr2.next <<
 	std::endl << std::endl;
 
 	Serializer a;
 
-	Data *reserialized_struct = a.unserialize(a.serialize(&ptr));
+	Data *ptr3 = a.unserialize(a.serialize(&ptr1));
 
 	std::cout << "Here is the reserialized structs:" <<
-				"\n\taddress: " << reserialized_struct <<
-				"\n\tname: " << reserialized_struct->name <<
-				"\n\tage: " << reserialized_struct->age <<
-				"\n\taddress next: " << reserialized_struct->next <<
+				"\n\taddress: " << ptr3 <<
+				"\n\tname: " << ptr3->name <<
+				"\n\tage: " << ptr3->age <<
 	std::endl;
 	std::cout << "\taddress ptr2: " << &ptr2 <<
 				"\n\tname: " << ptr2.name <<
 				"\n\tage: " << ptr2.age <<
-				"\n\taddress next: " << ptr2.next <<
 	std::endl << std::endl;
 
 	return (0);
